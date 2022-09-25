f6b2deb8078b141c4e3f86ad580355597b8246d9
PierceCoverage <110280405+PierceCoverage@users.noreply.github.com>
2022-09-25 11:52:04 -0500

    Update spoof.lua

--- spoof.lua
+++ spoof.lua
@@ -1,42 +1,35 @@
 local Players = game:GetService("Players")
-local function spoof()
-	local hum = Players.LocalPlayer.Character:WaitForChild("Humanoid")
+local hum = Players.LocalPlayer.Character:WaitForChild("Humanoid")
 
-	local SpoofTable = {
-		WalkSpeed = hum.WalkSpeed,
-		JumpPower = hum.JumpPower,
-	}
+local SpoofTable = {
+	WalkSpeed = hum.WalkSpeed,
+	JumpPower = hum.JumpPower,
+}
 
-	local __newindex
+local __index
+__index = hookmetamethod(game, "__index", function(t, k)
+    -- // Make sure it's trying to get our humanoid's ws/jp
+    if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
+        -- // Return spoof values
+        return SpoofTable[k]
+    end
 
-	__newindex = hookmetamethod(game, "__newindex", function(t, k, v)
-		-- // Make sure it's trying to set our humanoid's ws/jp
-		if not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower") then
-			-- // Add values to spoof table
-			SpoofTable[k] = v
+    -- //
+    return __index(t, k)
+end)
 
-			-- // Disallow the set
-			return
-		end
+-- // __newindex hook
+local __newindex
+__newindex = hookmetamethod(game, "__newindex", function(t, k, v)
+    -- // Make sure it's trying to set our humanoid's ws/jp
+    if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
+        -- // Add values to spoof table
+        SpoofTable[k] = v
 
-		-- //
-		return __newindex(t, k, v)
-	end)
-
-	local __index
-
-	__index = hookmetamethod(game, "__index", function(t, k, v)
-		if not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower") then
-			-- // Make sure it's trying to get our humanoid's ws/jp
-			if not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower") then
-				-- // Return spoof values
-				return SpoofTable[k]
-			end
-
-			-- //
-			return __index(t, k)
-		end
-	end)
-end
-
-spoof()
+        -- // Disallow the set
+        return v
+    end
+    
+    -- //
+    return __newindex(t, k, v)
+end)
