6aa773e84611aea5a3b21dcb74ac70ed855ba28d
PierceCoverage <110280405+PierceCoverage@users.noreply.github.com>
2022-09-25 11:54:46 -0500

    Update spoof.lua

--- spoof.lua
+++ spoof.lua
@@ -9,7 +9,7 @@ local SpoofTable = {
 local __index
 __index = hookmetamethod(game, "__index", function(t, k)
     -- // Make sure it's trying to get our humanoid's ws/jp
-    if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
+    if (not checkcaller() and t:IsA("Humanoid") and k == "JumpPower") then
         -- // Return spoof values
         return SpoofTable[k]
     end
@@ -22,7 +22,7 @@ end)
 local __newindex
 __newindex = hookmetamethod(game, "__newindex", function(t, k, v)
     -- // Make sure it's trying to set our humanoid's ws/jp
-    if (not checkcaller() and t:IsA("Humanoid") and (k == "WalkSpeed" or k == "JumpPower")) then
+    if (not checkcaller() and t:IsA("Humanoid") and k == "JumpPower") then
         -- // Add values to spoof table
         SpoofTable[k] = v
 
